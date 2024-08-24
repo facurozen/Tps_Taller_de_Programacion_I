749e6246b883536d6e3bcd4c638604709a258715
facurozen <106318798+facurozen@users.noreply.github.com>
2024-08-23 22:27:30 -0300

    Tp 2 terminado

--- Tp 2. Estrucura Secuencial.py
+++ Tp 2. Estrucura Secuencial.py
@@ -109,7 +109,6 @@ print("El resultado de la operación 12 % 5 es:",opreracion_17)
 print("--------------------------------------------------------------------")
 
 
-
 # Ejercicio 2
 
 numA = int(input("Ingrese un numero entero: "))
@@ -140,7 +139,6 @@ def promedio(notaA, notaB):
 print("El promedio de notas del alumno fue:",promedio(notaA,notaB))
 
 
-
 # Ejercicio 4
 
 edad = int(input("Ingrese la edad en años: "))
@@ -152,7 +150,6 @@ def convertir_edad_a_dias (edad):
 print(edad, "años, son:",convertir_edad_a_dias(edad),"días")   
 
 
-
 # Ejercicio 5
 
 precioMedicamento = float(input("Ingrese el precio del medicamento: "))
@@ -162,7 +159,6 @@ importeFinal = precioMedicamento - descuento
 print("El precio original del medicamento es de:",precioMedicamento)
 print("El monto del descuento del medicamento es de:",descuento)
 print("El importe final a pagar es:",importeFinal)
-
 
 
 # Ejercicio 6
@@ -181,7 +177,6 @@ print("La segunda persona aportó:", porcentaje2,"%")
 print("La tercera persona aportó:", porcentaje2,"%")
 
 
-
 # Ejercicio 7
 
 dineroInvertido = float(input("Ingrese el dinero invertido en el banco: "))
@@ -189,7 +184,6 @@ rendimientoMensual = (2 * dineroInvertido) / 100
 rendimientoTotal = rendimientoMensual * 6
 
 print("Si deja su dinero invertido en el banco, ganará un total de: $",rendimientoTotal)
-
 
 
 # Ejercicio 8
@@ -205,10 +199,96 @@ print(medidaMetros,"metros, equivalen a",conversionPulgadas,"pulgadas")
 print(medidaMetros,"metros, equivalen a",conversionPies,"pies")
 print(medidaMetros,"metros, equivalen a",conversionYardas,"yardas")
 
-"""
+
 
 # Ejercicio 9
 
 numVendedor = int(input("Ingrese el numero del vendedor: "))
+cantVentas = int(input("Ingrese la cantidad de ventas que realizó: "))
+valorTotalVentas = int(input("Ingrese el valor total de las ventas que realizó: "))
+salarioBase = 250000
+comisionXVentas = 50000 * cantVentas
+comisionTotalVentas = (5 * valorTotalVentas) / 100
 
+salarioTotal = int(salarioBase + comisionXVentas + comisionTotalVentas)
+
+print("El vendedor número",numVendedor,"debe recibir un salario de $",salarioTotal)
+
+
+# Ejercicio 10
+
+largo = float(input("Ingrese la medida del largo de su parcela, expresada en metros: "))
+ancho = float(input("Ingrese la medida del ancho de su parcela, expresada en metros: "))
+areaParcela = largo*ancho
+
+# Así calculo cuantos grupos de 2 quintales hay en la parcela, porque en 10 m2, hay 2 quintales.
+grupo_de_quintales_en_10m2 = areaParcela / 10
+
+# Así calculo la cantidad de quintales, ya que en 10 m2 hay dos quintales, y la variable grupo_de_quintales_en_10m2 va a tener cuantos bloques de 2 quintales hay en la parcela, por lo que lo multiplico por dos.
+cantQuintales = grupo_de_quintales_en_10m2 * 2
+
+print("En la parcela se pueden producir",cantQuintales,"quintales de trigo")
+
+
+# Ejercicio 11
+
+periodoEnSegundos = int(input("Ingrese un período de segundos: "))
+
+conversionDias = periodoEnSegundos // 86400 
+segundos_restantes_dias = periodoEnSegundos % 86400 # (Segundos restantes que no llegan a completar un día entero)
+conversionHoras = segundos_restantes_dias // 3600 # (Cantidad de segundos que tiene una hora)
+segundos_restantes_horas = segundos_restantes_dias % 3600
+conversionMinutos = segundos_restantes_horas // 60 # cantidad de segundos en un minuto
+segundos_restantes_minutos = segundos_restantes_horas % 60
+
+print(periodoEnSegundos,"segundos equivalen a:",conversionDias,"días,",conversionHoras,"horas,",conversionMinutos,"minutos y",segundos_restantes_minutos,"segundos")
+
+
+# Ejercicio 12
+
+cantDinero = int(input("Ingrese la cantidad de dinero que quiere retirar: "))
+
+billetes1000 = cantDinero // 1000
+restantes1000 = cantDinero % 1000
+
+billetes500 = restantes1000 // 500
+restantes500 = restantes1000 % 500
+
+billetes100 = restantes500 // 100
+restantes100 = restantes500 % 100
+
+billetes50 = restantes100 // 50
+restantes50 = restantes100 % 50
+
+billetes10 = restantes50 // 10
+restantes10 = restantes50 % 10
+
+billetes5 = restantes10 // 5
+restantes5 = restantes10 % 5
+
+# en este como ya no quedan billetes de menor valor, y tampoco hay monedas, no calculo el resto
+billetes1 = restantes5 // 1
+
+print("El cajero debe entregar:",billetes1000, "billetes de $1000,",billetes500,"billetes de $500,",billetes100,"billetes de $100,",billetes50,"billetes de $50,",billetes10,"billetes de $10,",billetes5,"billetes de $5,", billetes1," billetes de $1")
+
+
+# Ejercicio 13
+
+numBinario = int(input("Ingrese un numero binario de 4 cifras. Recordá que un numero binario, solo acepta 0 y 1: "))
+
+# este orden es contando desde la izquierda de todo, cuando lo paso de decimal a binario, sería el que esta elevado a la mayor potencia
+primerNumero = numBinario // 1000
+
+# el % 10 lo uso para obtener el numero de la derecha, ya que cuando divido el numero binario por 100, me quedan dos numero, si yo hago el % 10 de esos dos numeros, me da el digito de la derecha, y en ese caso, busco el digito de la derecha, porque el de la izquierda es el que calcule ariba, al hacer // 1000, me sacó los ultimo 3 numeros, por lo tanto, al hacer // 100, obtengo 2 numeros, pero como el de la izquierda ya lo tengo, hago % 10, y así me da el de la derecha.
+# % 10 siempre me va a dar como resultado el numero de la derecha
+segundoNumero = (numBinario // 100) % 10
+
+tecerNumero = (numBinario // 10) % 10
+
+cuartoNumero = numBinario % 10
+
+numDecimal = (primerNumero * 2**3) + (segundoNumero * 2**2) + (tecerNumero * 2 **1) + (cuartoNumero*2**0)
+print("El número binario",numBinario,"equivale al número decimal:",numDecimal)
+
+"""
 
