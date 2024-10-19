bc40b79083049f1bf05d1ed240882f2f66463918
facurozen <106318798+facurozen@users.noreply.github.com>
2024-10-19 15:23:35 -0300

    Add files via upload

--- Tp 7. Estructuras De Datos - Listas.py
+++ Tp 7. Estructuras De Datos - Listas.py
@@ -87,10 +87,46 @@ else :
 
 print("La suma de la lista de números es:",suma)
 
-
-
+"""
 # Ejercicio 3
 
+def invertirLista(vec) :
+    largo = len(vec)
+    nuevaLista = []
+    for i in range(largo-1,-1,-1) :
+
+        nuevaLista.append(vec[i])
+
+    return nuevaLista 
+
+
+def determinarCapicua(vec): 
+    listaInvertida = invertirLista(vec) # USO LA FUNCIÓN QUE CREE EN EL EJERCICIO 5
+
+    if listaInvertida == vec :
+        capicua = True
+
+    else :
+        capicua = False
+
+    return capicua
+
+numA = int(input("Ingrese el primer número: "))
+numB = int(input("Ingrese el segundo número, para determinar el rango de valores validos: "))
+
+vecNumeros = ingresarNumerosLista(numA,numB)
+
+print()
+capicua = determinarCapicua(vecNumeros)
+
+if capicua == True :
+    print("La lista es capicúa")
+
+else :
+    print("La lista NO es capicúa")
+
+
+"""
 # Ejercicio 4
 
 def contarApariciones(vec,num) :
@@ -115,14 +151,56 @@ print()
 contadorApariciones = contarApariciones(vecNumeros,num_a_buscar)
 print("El número",num_a_buscar,"está",contadorApariciones,"veces en la lista")
 
-"""
 
+"""
 # Ejercicio 5
 
+def invertirLista(vec) :
+    largo = len(vec)
+    nuevaLista = []
+    for i in range(largo-1,-1,-1) :
+
+        nuevaLista.append(vec[i])
+
+    return nuevaLista 
+
+"""   
 
 numA = int(input("Ingrese el primer número: "))
 numB = int(input("Ingrese el segundo número, para determinar el rango de valores validos: "))
 
 vecNumeros = ingresarNumerosLista(numA,numB)
 
-# HACER UN FOR QUE LA RECORRA AL REVES LA LISTA, Y A LA NUEVA LISTA PONERLE EL VALOR DE VEC[I]
+vecNumerosInvertido = invertirLista(vecNumeros)
+print()
+print("La lista original es:", vecNumeros)
+print("La lista invertida es: ",vecNumerosInvertido)
+
+
+# Ejercicio 6
+
+def obtenerPosicion(valor,vec) :
+    largo = len(vec)
+    nuevaLista = []
+    pos = -1
+
+    for i in range(largo):
+        if valor == vecNumeros[i] :
+            pos = i
+            nuevaLista.append(pos)
+    
+    return nuevaLista
+
+
+numA = int(input("Ingrese el primer número: "))
+numB = int(input("Ingrese el segundo número, para determinar el rango de valores validos: "))
+vecNumeros = ingresarNumerosLista(numA,numB)
+
+print()
+valor = float(input("Ingrese el número que desea saber en que posición/es se encuentra en la lista: "))
+listaPosiciones = obtenerPosicion(valor,vecNumeros)
+
+print()
+print("Lista de posiciones del valor",valor,"en la lista original:",listaPosiciones)
+
+"""
