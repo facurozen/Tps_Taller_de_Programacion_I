7a309ed45ac6b8a3aaa6f344d6d8ea9407711903
facurozen <106318798+facurozen@users.noreply.github.com>
2024-10-25 20:09:16 -0300

    avances en la estructura

--- Tp Final.py
+++ Tp Final.py
@@ -28,6 +28,7 @@ def validarFechaMes () :
     while mes <=0 or mes > 12 :
         print("ERROR, el mes debe estar entre 1 y 12")
         mes = int(input("Ingrese el mes en el que se realizó la venta: "))
+    return mes
 
 
 def validarFechaDia(mes) :
@@ -47,8 +48,11 @@ def validarFechaDia(mes) :
             print("ERROR, el día debe ser mayor a 0 y menor a 30")
             fechaDia = int(input("Ingrese el día que se realizó la venta: "))
 
+    return fechaDia
 # Función para cargar los datos de cada venta
 def cargarDatosVentas(cant) :
+
+    vecVentas = []
 
     idProducto = []
     categoria = []
@@ -59,7 +63,8 @@ def cargarDatosVentas(cant) :
  
 
     for i in range(cant) :
-        print("N° VENTA:",i)
+        print()
+        print("N° VENTA:",i +1)
         print()
         
         id = validarId()
@@ -80,16 +85,57 @@ def cargarDatosVentas(cant) :
         fechaD = validarFechaDia(fechaM)
         fechaDia.append(fechaD)
 
+        venta = [id,cat,prec,cantVen,fechaM,fechaD]
+        vecVentas.append(venta)
 
+    return vecVentas
+
+def calcularTotalIngresos(vec) :
+    largo = len(vec)
+
+    acumIngresos = 0
+
+    for i in range(largo) :
+        
+        vec[i precio] * vec[i cantidad]
+
+    return acumIngresos
+
+
+def obtenerCantProductosCategoria(vec,cat) :
+    largo = len(vec)
+    cantTotal = 0  
+
+    for i in range(largo) :
+        if cat == vec[i categoria] :
+            cantTotal += vec[i cantVendida]
+
+    return cantTotal
+
+
+
+# PROGRAMA PRINCIPAL
 
 # Se le pide al usuario que ingrese la cantidad de ventas que realizó en el último mes. Al ser ventas, el tipo de dato es entero, no puede ser decimal
 cantVentas = int(input("Ingrese la cantidad de ventas realizadas en el último mes: "))
 
 # Al tener que ingresar como minimo 10 ventas, si el usuario ingresa menos, se le vuelve a pedir
-while cantVentas < 10 :
+while cantVentas < 0 :
     print("ERROR, la cantidad de ventas mínima debe ser 10")
     cantVentas = int(input("Ingrese la cantidad de ventas realizadas en el último mes: "))
 
-cargarDatosVentas(cantVentas)
+vecVentas = cargarDatosVentas(cantVentas)
+
+categoriaABuscar = input("Ingrese la categoría del producto que desea saber cuantos productos se han vendido en total: ")
+
+print()
+
+totalIngresos = calcularTotalIngresos(vecVentas)
+print("En el último mes, la empresa a generado ingresos de $",totalIngresos)
+
+print()
+
+totalProductosXCategoria = obtenerCantProductosCategoria(vecVentas,categoriaABuscar)
+print("De la categoria",categoriaABuscar,"se han vendido",totalProductosXCategoria,"productos")
 
 
